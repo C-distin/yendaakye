c1be76992a66c0f99a302e7ab8eeb86a532e49a4
Tricky <trickya23@outlook.com>
2025-05-30 11:43:52 +0000

    fix supabase

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,5 +1,5 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
 import * as schema from './schema';
 
 // Check if the DATABASE_URL environment variable exists
@@ -7,13 +7,11 @@ if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL environment variable is not set');
 }
 
-// Create a new PostgreSQL connection pool
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+// Create a new PostgreSQL client
+const client = postgres(process.env.DATABASE_URL, { prepare: false });
 
-// Create a Drizzle instance with the pool and schema
-export const db = drizzle(pool, { schema });
+// Create a Drizzle instance with the client and schema
+export const db = drizzle(client, { schema });
 
-// Export the pool to be able to end it when needed
-export { pool }; 
+// Export the client to be able to end it when needed
+export { client }; 
